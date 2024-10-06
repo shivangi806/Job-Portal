40277d10f40af98556cf03a5feefd702c2d2b813
Shivangi Chandra <shivangichandra806@gmail.com>
2024-10-06 12:35:07 +0530

    Update Navbar.jsx

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "https://job-portal-cyan-one.vercel.app/logout",
         {
           withCredentials: true,
         }
