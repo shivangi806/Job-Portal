7034f017ca01b111e35952c22b10f1b3e8a04d29
Shivangi Chandra <shivangichandra806@gmail.com>
2024-10-06 12:20:46 +0530

    url Application.jsx

--- frontend/src/components/Application/Application.jsx
+++ frontend/src/components/Application/Application.jsx
@@ -35,7 +35,7 @@ const Application = () => {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/application/post",
+        "https://job-portal-cyan-one.vercel.app/post",
         formData,
         {
           withCredentials: true,
